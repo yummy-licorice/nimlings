1841d9db262505966c61c8f62034409e0e7c4d78
Luke <Infinitybeond1@protonmail.com>
2022-07-03 16:37:06 -0500

    3rd excercise

---
+++ src/exercises/ex_3.nim
@@ -0,0 +1,15 @@
+# More variables 
+# Another type of variable is defined with let 
+# A variable defined with let is immutable meaning it can't be changed 
+# 
+# let x = 10 
+# x = 11 
+#
+# The above code will throw an error because you cant change the value of a variable defined with let 
+# 
+# Change the code so that x = 10
+let x = 10 
+x = 11 
+
+assert x == 10 
+echo x
